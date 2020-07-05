0cab96668f854bacdf316ba87c356c99b8e30b29
jiwomdf <mdf.jiwo@gmail.com>
2020-07-05 19:45:17 +0700

    -setting up googleMap -setting up service

--- app/src/main/java/com/programmergabut/larikuy/ui/MainActivity.kt
+++ app/src/main/java/com/programmergabut/larikuy/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.programmergabut.larikuy.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.programmergabut.larikuy.R
+import com.programmergabut.larikuy.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -27,4 +31,16 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
+    }
+
 }
